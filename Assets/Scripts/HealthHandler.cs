bb8a1c1dc9f687c07b870cc7f87c1d8ecb66e5b3
Keith Guyah <keithguyah@gmail.com>
2019-02-06 14:46:21 -0500

    Characters Health added. -Cant get the stupid collision detection to work.

---
+++ Assets/Scripts/HealthHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthHandler : MonoBehaviour
+{
+    public bool _isPlayer = false;
+    public bool _isShield = false;
+    public int _maxHP=1;
+    public int _currentHP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentHP = _maxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_currentHP>_maxHP)
+        {
+            _currentHP = _maxHP;
+        }        
+    }
+    public void takeDamage(int damage)
+    {
+        _currentHP -= damage;
+
+        if (_currentHP <= 0)
+        {
+            if (_isPlayer == true)
+            {
+                Destroy(gameObject);
+                death();
+            }
+            else if (_isShield == true)
+            {
+                //need to workout mechanics for if sheild health is equal to zero.
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    void death()
+    {
+
+        //put restart level here.
+        //Application.LoadLevel(Application.loadedLevel);
+    }
+}
